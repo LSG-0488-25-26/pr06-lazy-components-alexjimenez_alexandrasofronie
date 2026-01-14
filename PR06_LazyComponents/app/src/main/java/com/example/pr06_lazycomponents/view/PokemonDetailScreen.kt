f4adfcbdf14ea5c1a47aeaaab271fe3b4b502e6f
Alesa <alexandrateodora.sofronie@gracia.lasalle.cat>
2026-01-14 17:37:13 +0100

    final commit alex

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/PokemonDetailScreen.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/PokemonDetailScreen.kt
@@ -1,6 +1,5 @@
 package com.example.pr06_lazycomponents.view
 
-import androidx.benchmark.traceprocessor.Row
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
