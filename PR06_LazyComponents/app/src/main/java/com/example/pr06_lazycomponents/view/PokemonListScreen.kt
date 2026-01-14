6e14961c048d8312d2a1e45e2d7714a45f93b368
Alesa <alexandrateodora.sofronie@gracia.lasalle.cat>
2026-01-14 15:02:52 +0100

    Pantalla base pokemon list

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/PokemonListScreen.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/PokemonListScreen.kt
@@ -1,2 +1,24 @@
 package com.example.pr06_lazycomponents.view
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.pr06_lazycomponents.model.Pokemon
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+
+@Composable
+fun PokemonListScreen(
+    onPokemonClick: (Pokemon) -> Unit
+) {
+    Column {
+        Text(
+            text = "Pokédex",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+}
